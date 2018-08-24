bc375aafac700752dedde516313c0c27b14dbd6e
Bryon <30876973+nukeguard@users.noreply.github.com>
2018-08-24 07:03:13 -0400

    Initial Commit for Deuterium Fusion Reactors

---
+++ Data/Scripts/PlasmaSparks/PlasmaSparks.cs
@@ -0,0 +1,106 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using Sandbox.Game;
+using SpaceEngineers.Game;
+using System;
+using System.Collections.Generic;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.Entity;
+using VRage.ModAPI;
+using VRageMath;
+
+namespace PlasmaField
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Reactor), false, "LBFusionReactorT1", "SBFusionReactorT1",
+        "LBFusionReactorT2", "SBFusionReactorT2", "LBFusionReactorT3", "SBFusionReactorT3", "LBFusionReactorT4",
+        "SBFusionReactorT4", "LBFusionReactorT5", "SBFusionReactorT5")]
+    public class ReactorRotatorGameLogic : MyGameLogicComponent
+    {
+        private const float MAX_RATE = (float)Math.PI * 2; // 360 deg/sec
+        IMyReactor _reactor;
+                
+        public override void OnAddedToContainer()
+        {
+            if (MyAPIGateway.Utilities.IsDedicated) return;
+            _reactor = (IMyReactor)Entity;
+            _reactor.IsWorkingChanged += Reactor_IsWorkingChanged;
+            Reactor_IsWorkingChanged(_reactor);
+        }
+
+        public override void OnBeforeRemovedFromContainer()
+        {
+            if (MyAPIGateway.Utilities.IsDedicated) return;
+            _reactor.IsWorkingChanged -= Reactor_IsWorkingChanged;
+            _reactor = null;
+            _effect?.Stop();
+                if (_effect != null)
+                    MyParticlesManager.RemoveParticleEffect(_effect);
+        }
+
+        private void Reactor_IsWorkingChanged(IMyCubeBlock obj)
+        {
+            UpdateParticleEffect();
+            NeedsUpdate = _reactor.IsWorking ? MyEntityUpdateEnum.EACH_FRAME : MyEntityUpdateEnum.NONE;
+        }
+
+        private MyParticleEffect _effect;
+        private IMyModel _effectCachedModel;
+        private MatrixD? _effectMatrix;
+
+      
+        private void UpdateParticleEffect()
+        {
+            if (_effectCachedModel != _reactor.Model)
+            {
+                _effectCachedModel = _reactor.Model;
+                var tmp = new Dictionary<string, IMyModelDummy>();
+                _effectCachedModel?.GetDummies(tmp);
+                _effectMatrix = tmp.GetValueOrDefault("subpart_PlasmaParticle")?.Matrix; // empty for particle
+            }
+            if (_reactor.IsWorking && _effectMatrix.HasValue)
+            {
+                var fractionalOutput = _reactor.CurrentOutput / _reactor.MaxOutput;
+                var dTheta = MAX_RATE * fractionalOutput * MyEngineConstants.PHYSICS_STEP_SIZE_IN_SECONDS;
+                _effectMatrix = _effectMatrix.Value * MatrixD.CreateRotationY(dTheta);
+                if (_effect == null)
+                    MyParticlesManager.TryCreateParticleEffect("PlasmaSparks", out _effect); // particle subtype
+                _effect.WorldMatrix = _effectMatrix.Value * _reactor.WorldMatrix;
+                _effect.Velocity = _reactor.CubeGrid.Physics?.GetVelocityAtPoint(_effect.WorldMatrix.Translation) ?? Vector3.Right; // rotation
+            }
+            else
+            {
+                _effect?.Stop();
+                if (_effect != null)
+                    MyParticlesManager.RemoveParticleEffect(_effect);
+                _effect = null;
+            }
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            if (MyAPIGateway.Utilities.IsDedicated) return;
+            UpdateParticleEffect();
+            MyEntitySubpart subpart;
+            if (_reactor != null && _reactor.TryGetSubpart("PlasmaParticle", out subpart))
+            {
+                var sub = (IMyEntity)subpart;
+                var fractionalOutput = (_reactor.CurrentOutput + 2) / 800;
+                var dTheta = MAX_RATE * fractionalOutput * MyEngineConstants.PHYSICS_STEP_SIZE_IN_SECONDS;
+                subpart.SetEmissiveParts("PlasmaWave", Color.Blue, fractionalOutput);
+                sub.LocalMatrix = sub.LocalMatrix * Matrix.CreateRotationY(dTheta);
+            }
+
+           if (_reactor != null && _reactor.TryGetSubpart("PlasmaParticle2", out subpart))
+            {
+                var sub = (IMyEntity)subpart;
+                var fractionalOutput = (_reactor.CurrentOutput + 1) / 800;
+                var dTheta = MAX_RATE * fractionalOutput * MyEngineConstants.PHYSICS_STEP_SIZE_IN_SECONDS;
+                subpart.SetEmissiveParts("PlasmaWave", Color.White, fractionalOutput);
+                sub.LocalMatrix = sub.LocalMatrix * Matrix.CreateRotationY(dTheta);
+            }
+
+        }
+    }
+}
